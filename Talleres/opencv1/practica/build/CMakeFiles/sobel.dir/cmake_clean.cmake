63d16e74f966939ffa742d5e756f92ddb9f38458
hpc course <hpc@gmail.com>
2016-04-22 16:45:46 -0500

    se añadio todo :v

---
+++ Talleres/opencv1/practica/build/CMakeFiles/sobel.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/sobel.dir/sobel.cc.o"
+  "sobel.pdb"
+  "sobel"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/sobel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
